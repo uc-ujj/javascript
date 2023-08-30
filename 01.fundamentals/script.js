562f9fb3db41eb721a9d1cff5ecda12ded5d63bb
uc-ujj <ujjawal.gupta@ucertify.com>
2023-08-30 13:19:53 +0530

    js values

---
+++ 01.fundamentals/script.js
@@ -0,0 +1,18 @@
+/** Three pillars of JavaScript
+ * Types / Coercion
+ * Scope / Closures
+ * this / Prototypes
+ */
+
+// Values
+42, 2.15, 'Hello, Ujjawal.'
+
+true
+false
+null 
+undefined
+
+[1, 2, 3]
+{name: "Ujjawal"}
+
+
