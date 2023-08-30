d11f1c0d940bff18dc911ae73a6c446027394b3f
uc-ujj <ujjawal.gupta@ucertify.com>
2023-08-30 13:28:15 +0530

    operations and check type

--- 01.fundamentals/script.js
+++ 01.fundamentals/script.js
@@ -5,14 +5,34 @@
  */
 
 // Values
-42, 2.15, 'Hello, Ujjawal.'
+// 42, 2.15, "Hello, Ujjawal.";
 
-true
-false
-null 
-undefined
+/*
+true;
+false;
+null;
+undefined[(1, 2, 3)];
+{
+  name: "Ujjawal";
+*/
 
-[1, 2, 3]
-{name: "Ujjawal"}
+// Operations
+
+/**
+ * 3 + 4
+ * 43 - 23
+ * 'Ujjawal' + 'Gupta'
+ * !false
+ * 3.0 == 3
+ * 3 < 4
+ * true || false
+ * 
+ */
+
+// To check the type of anything in JavaScript
+typeof(23)
+typeof("Ujjawal")
+typeof([1, 2, 3, 4])
+typeof({lastName: 'Gupta'})
 
 
